47345dd5336e90b1348322612ed0a5b4b46dde65
mchl02 <matthewchlee02@gmail.com>
2019-01-19 11:45:50 -0500

    Update MainlyViewController.swift

--- Tic Tac Toe/MainlyViewController.swift
+++ Tic Tac Toe/MainlyViewController.swift
@@ -1,1 +1,34 @@
 //second view controller
+
+import UIKit
+
+class MainlyViewController: UIViewController {
+
+    @IBOutlet var Normal: UIButton!
+    @IBOutlet var Expert: UIButton!
+    
+    @IBAction func NormalPressed(_ sender: Any) {
+        wasTheButtonPressed = false
+    }
+    
+    @IBAction func ExpertPressed(_ sender: Any) {
+        wasTheButtonPressed = true
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
